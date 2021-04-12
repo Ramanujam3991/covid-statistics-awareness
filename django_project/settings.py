ee5df1b5980dc40e83303fb7b7ba1fb0bc2e8d99
Ramanujam3991 <ramanujam3991@gmail.com>
2021-04-12 16:13:53 -0400

    making base compatible for heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 #  Add configuration for static files storage using whitenoise
